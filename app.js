490b068896fc5a0371013899bbf95a54b586beb8
Cruz Diego <diegohiller754@gmail.com>
2025-08-08 16:24:13 -0300

    Mitad del los modelos hechos, sin testeos

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 import express from "express";
 import dotenv from 'dotenv' ;
 import sequelize from "./src/config/database.js";
+import User from './src/models/user.model.js';
+import Task from './src/models/task.model.js';
+import userRoutes from './src/routes/user.routes.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(userRoutes);
 app.use(express.json());
 
 (async () => {
-    try{
-        await sequelize.authenticate();
-        console.log("Conexion a la base de datos establecida con Exito")
-    } catch (error) {
-        console.log("No se estableció la conexión con la base de datos", error);
-    };
-     
-});
+  try {
+    await sequelize.authenticate();
+    console.log('Tenemos conexión a la base de datos.');
+
+    await sequelize.sync({ alter: true }); 
+    console.log('Tablas sincronizadas correctamente.');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+})();
 
 app.listen(PORT, () => {
       console.log(`Servidor escuchando en http://localhost:${PORT}`);
     });
+
+
+   
